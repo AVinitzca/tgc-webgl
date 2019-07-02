750459ba933bf5129bef59f4dca95a60c8c5e9a7
Ronan Vinitzca <ronan.vinitzca@globant.com>
2019-07-02 18:26:34 -0300

    Appfix

--- example-boxes/app.js
+++ example-boxes/app.js
@@ -2,26 +2,31 @@ function App(){}
 
 App.prototype.initialize = function()
 {
-    this.cubeColor = new Box();
-    this.cubeColor.addTexture("textureOne", this.textures['lava']);
+    this.box = new Box();
+    this.box.addTexture("textureOne", this.textures['lava']);
+    this.box.setShader(this.shaders['brolge']);
 
-    this.camera = {position: new Vector3(500.0), lookAt: Vector3.Zero(), up: new Vector3(0.0, 1.0, 0.0)};
+    this.boxScaling = Matrix4.scaling(new Vector3(100.0, 100.0, 100.0));
+    this.angle = 0.0;
+
+    this.camera = {position: new Vector3(300.0), lookAt: Vector3.Zero(), up: new Vector3(0.0, 1.0, 0.0)};
     Renderer.setCamera(this.camera);
 }
 
 App.prototype.update = function(elapsedTime)
 {
-
+    this.box.transform = Matrix4.multiply(Matrix4.rotationY(this.angle), this.boxScaling);
+    this.angle += 2.0 * elapsedTime;
 }
 
 App.prototype.render = function()
 {
-    //this.cubeColor.render();
+    this.box.render();
 }
 
 App.prototype.dispose = function()
 {
-    this.cubeColor.dispose();
+    this.box.dispose();
 }
 
 
